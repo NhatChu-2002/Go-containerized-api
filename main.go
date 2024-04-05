a8711d3411c39d65ed028c89e6074e21f83505a5
NhatChu <chunhat321123@gmail.com>
2024-04-05 16:53:18 +0700

    edit dockerfile

--- main.go
+++ main.go
@@ -3,16 +3,30 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	// "database/sql"
+	// "encoding/json"
+	"fmt"
+	// _ "github.com/go-sql-driver/mysql"
+	"log"
+	// "github.com/gin-gonic/gin"
 )
 
-func main() {
-	r := gin.Default()
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
 
-	r.GET("/api/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello world!",
-		})
-	})
-	r.Run()
+func main() {
+
+	// r := gin.Default()
+
+	// r.GET("/api/hello", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "Hello world!",
+	// 	})
+	// })
+	// r.Run()
+
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
